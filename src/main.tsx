629c22a3b2892274f40e43385263d0987b650e9a
srihanavirneni <srihanavirneni@gmail.com>
2024-10-19 00:06:02 -0400

    installed more dependencies and imported previous project to here

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import App from './App.tsx';
 
 import { HashRouter } from 'react-router-dom';
 
+import "@radix-ui/themes/styles.css";
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/fbla-gym-project/'
-})
+    plugins: [react()],
+    base: '/fbla-gym-project/',
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+});
