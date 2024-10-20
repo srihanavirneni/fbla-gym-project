934746f65d799603079d27144a45a03cc147e96e
srihanavirneni <srihanavirneni@gmail.com>
2024-10-20 18:26:54 -0400

    updated images

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -10,6 +10,13 @@ import AboutInfo from '../Home/AboutInfo';
 import AboutHeader from './AboutHeader';
 
 import placeholder from '../../assets/images/placeholder.png';
+
+import gym0 from '../../assets/images/gymphoto0.png';
+import gym1 from '../../assets/images/gymphoto1.png';
+import gym2 from '../../assets/images/gymphoto2.png';
+import gym3 from '../../assets/images/gymphoto3.png';
+import gym4 from '../../assets/images/gymphoto4.png';
+
 import './About.css';
 
 const ImageHolder = (props: any) => {
@@ -24,23 +31,23 @@ const ImageHolder = (props: any) => {
 const About = () => {
     const carouselContent = [
         {
-            image: placeholder,
+            image: gym0,
             description: 'gym picture 1',
         },
         {
-            image: placeholder,
+            image: gym1,
             description: 'gym picture 2',
         },
         {
-            image: placeholder,
+            image: gym2,
             description: 'gym picture 3',
         },
         {
-            image: placeholder,
+            image: gym3,
             description: 'gym picture 4',
         },
         {
-            image: placeholder,
+            image: gym4,
             description: 'gym picture 5',
         },
     ];
@@ -51,18 +58,20 @@ const About = () => {
             <div className="about-page__gym-carousel">
                 <Carousel className="w-full">
                     <CarouselContent>
-                        {Array.from({ length: 5 }).map((_, index) => (
-                            <CarouselItem key={index}>
-                                <ImageHolder
-                                    className="gym-carousel__item-content"
-                                    image={carouselContent[index].image}
-                                    alt="picture of gymnasium"
-                                    description={
-                                        carouselContent[index].description
-                                    }
-                                />
-                            </CarouselItem>
-                        ))}
+                        {Array.from({ length: carouselContent.length }).map(
+                            (_, index) => (
+                                <CarouselItem key={index}>
+                                    <ImageHolder
+                                        className="gym-carousel__item-content"
+                                        image={carouselContent[index].image}
+                                        alt="picture of gymnasium"
+                                        description={
+                                            carouselContent[index].description
+                                        }
+                                    />
+                                </CarouselItem>
+                            )
+                        )}
                     </CarouselContent>
                     <CarouselPrevious />
                     <CarouselNext />
