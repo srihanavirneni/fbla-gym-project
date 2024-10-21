214098175160dd36623408e84855a9ff4a27478d
srihanavirneni <srihanavirneni@gmail.com>
2024-10-21 14:49:38 -0400

    added an calendar

--- src/components/layouts/EventCalendar/EventCalendar.tsx
+++ src/components/layouts/EventCalendar/EventCalendar.tsx
@@ -1,7 +1,31 @@
+import { ScheduleXCalendar, useCalendarApp } from '@schedule-x/react';
+import { createViewWeek } from '@schedule-x/calendar';
+import { createCurrentTimePlugin } from '@schedule-x/current-time';
+import '@schedule-x/theme-default/dist/calendar.css';
+
 import './EventCalendar.css';
 
 const EventCalendar = () => {
-    return <div>EventCalendar</div>;
+    const calendar = useCalendarApp({
+        views: [createViewWeek()],
+        events: [
+            {
+                id: 1,
+                title: 'My new event',
+                start: '2024-10-21 00:00',
+                end: '2024-10-21 02:00',
+            },
+        ],
+        plugins: [
+            createCurrentTimePlugin()
+        ]
+    });
+
+    return (
+        <div className="event-calendar">
+            <ScheduleXCalendar calendarApp={calendar} />
+        </div>
+    );
 };
 
 export default EventCalendar;
