5c4c4be8d0d60afefdcf815ad00d07f883616f9e
srihanavirneni <srihanavirneni@gmail.com>
2025-03-02 19:01:46 -0500

    lazy loading pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
+import React, { Suspense } from 'react';
 
 import ScrollToTop from './components/function/ScrollToTop';
 
 import MainNavigation from './components/layouts/Navigation/MainNavigation';
 import MainFooter from './components/layouts/Footer/MainFooter';
 
-import Home from './pages/Home/Home';
-import About from './pages/About/About';
-import Status from './pages/Status/Status';
-import Events from './pages/Events/Events';
-import Contact from './pages/Contact/Contact';
-import Calendar from './pages/Calendar/Calendar';
-import Transaction from './pages/Transaction/Transaction';
+// import Home from './pages/Home/Home';
+const Home = React.lazy(() => import('./pages/Home/Home'));
+const About = React.lazy(() => import('./pages/About/About'));
+const Status = React.lazy(() => import('./pages/Status/Status'));
+const Events = React.lazy(() => import('./pages/Events/Events'));
+const Contact = React.lazy(() => import('./pages/Contact/Contact'));
+const Calendar = React.lazy(() => import('./pages/Calendar/Calendar'));
+const Transaction = React.lazy(() => import('./pages/Transaction/Transaction'));
 
 import CreateEvent from './pages/CreateEvent/CreateEvent';
 import Booking from './pages/Booking/Booking';
@@ -24,28 +26,36 @@ const App = () => {
             <ScrollToTop />
             <MainNavigation />
             <main className="app-content">
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="about" element={<About />} />
-                    <Route path="status" element={<Status />} />
-                    <Route path="events" element={<Events />} />
-                    <Route path="contact" element={<Contact />} />
-                    <Route path="calendar" element={<Calendar />} />
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="about" element={<About />} />
+                        <Route path="status" element={<Status />} />
+                        <Route path="events" element={<Events />} />
+                        <Route path="contact" element={<Contact />} />
+                        <Route path="calendar" element={<Calendar />} />
 
-                    <Route path="events/create" element={<CreateEvent />} />
-                    <Route
-                        path="events/create/:date"
-                        element={<CreateEvent />}
-                    />
-                    <Route path="events/payment/:id" element={<Booking />} />
-                    <Route path="events/payment/:id/:path" element={<Booking />} />
-                    <Route
-                        path="events/payment/:id/transaction/:seats"
-                        element={<Transaction />}
-                    />
+                        <Route path="events/create" element={<CreateEvent />} />
+                        <Route
+                            path="events/create/:date"
+                            element={<CreateEvent />}
+                        />
+                        <Route
+                            path="events/payment/:id"
+                            element={<Booking />}
+                        />
+                        <Route
+                            path="events/payment/:id/:path"
+                            element={<Booking />}
+                        />
+                        <Route
+                            path="events/payment/:id/transaction/:seats"
+                            element={<Transaction />}
+                        />
 
-                    <Route path="*" element={<Navigate to="/" />} />
-                </Routes>
+                        <Route path="*" element={<Navigate to="/" />} />
+                    </Routes>
+                </Suspense>
             </main>
             <MainFooter />
         </div>
---
+++ src/pages/Contact/Test.tsx
@@ -0,0 +1,14 @@
+import './Test.css';
+
+const Test = () => {
+    return <div className='test'>
+        <h1>
+            We are getting it down
+        </h1>
+        <h2>
+            Typescript is a great language
+        </h2>
+    </div>;
+};
+
+export default Test;
